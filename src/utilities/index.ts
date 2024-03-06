dfc340ad7352e054874e1f891cb09f04d80787fe
satanmourner <mahmoudisanaz59@gmail.com>
2024-03-06 13:34:06 +0330

    refactor: add some utilities

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -3,3 +3,8 @@ export function decodeJwtPayload(jwt: string): any {
   const token = atob(jwt);
   return JSON.parse(atob(token.split('.')[1]));
 }
+
+//Texts
+export const beautifyText = (str: string) => {
+  return (str.charAt(0).toUpperCase() + str.slice(1)).replaceAll('_', ' ');
+};
