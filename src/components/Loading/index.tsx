6cd31069df3da0409315c39ad112fc41b5cb7d4e
satanmourner <mahmoudisanaz59@gmail.com>
2024-03-06 10:12:25 +0330

    feat: loading general component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+import { LoadingProps } from './index.types';
+
+const Loading: React.FC<LoadingProps> = ({
+  show,
+  message,
+  animation = 'border',
+  role = 'status',
+  variant = 'primary',
+  ...rest
+}) => {
+  if (show) {
+    return (
+      <div className="h-100 d-flex flex-column gap-5 align-items-center justify-content-center">
+        {message && <h5>{message}</h5>}
+        <Spinner animation={animation} role={role} variant={variant} {...rest} />
+      </div>
+    );
+  } else {
+    return <></>;
+  }
+};
+
+export default Loading;
