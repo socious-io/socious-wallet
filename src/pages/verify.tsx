c69a6667eed8f07bce33151c2a8ab235f850333f
jeyem <me@e-mahmoudi.me>
2024-03-06 15:12:13 +0330

    reset session when it's need

--- src/pages/verify.tsx
+++ src/pages/verify.tsx
@@ -9,6 +9,11 @@ import { useNavigate } from 'react-router-dom';
 
 const FLAG_KEY = 'submitted_kyc';
 
+const startKYC = async (did: string, session: string) => {
+  const headers = { 'x-api-key': config.BACKUP_AGENT_API_KEY };
+  return axios.post(`${config.BACKUP_AGENT}/verify/start`, { session, did }, { headers });
+};
+
 function Verify() {
   const { did, credentials, verification } = useAppState();
   const [submitted, setSubmitted] = useState(localStorage.getItem(FLAG_KEY) ? true : false);
@@ -27,15 +32,7 @@ function Verify() {
             localStorage.setItem(FLAG_KEY, `${true}`);
             localStorage.setItem('session', response.verification.id);
             setSubmitted(true);
-            const headers = { 'x-api-key': config.BACKUP_AGENT_API_KEY };
-            await axios.post(
-              `${config.BACKUP_AGENT}/verify/start`,
-              {
-                session: response.verification.id,
-                did: response.verification.vendorData,
-              },
-              { headers },
-            );
+            await startKYC(did.methodId, response.verification.id);
           }
         },
       });
@@ -61,22 +58,31 @@ function Verify() {
     if (did && submitted && verification === null) {
       const headers = { 'x-api-key': config.BACKUP_AGENT_API_KEY };
       const checkStatus = () => {
-        axios.get(`${config.BACKUP_AGENT}/verify/${did.methodId}/status`, { headers }).then((r) => {
-          switch (r.data.verification?.status) {
-            case 'declined':
-            case 'expired':
-            case 'abandoned':
-              localStorage.removeItem(FLAG_KEY);
-              setSubmitted(false);
-              break;
-            case 'approved':
-              const url = new URL(r.data.connection.url);
-              // need to clear messages before redirect
-              dispatch({ type: 'SET_NEW_MESSAGE', payload: [] });
-              navigate(`${url.pathname}${url.search}`);
-              break;
-          }
-        });
+        axios
+          .get(`${config.BACKUP_AGENT}/verify/${did.methodId}/status`, { headers })
+          .then((r) => {
+            switch (r.data.verification?.status) {
+              case 'declined':
+              case 'expired':
+              case 'abandoned':
+                localStorage.removeItem(FLAG_KEY);
+                setSubmitted(false);
+                break;
+              case 'approved':
+                const url = new URL(r.data.connection.url);
+                // need to clear messages before redirect
+                dispatch({ type: 'SET_NEW_MESSAGE', payload: [] });
+                navigate(`${url.pathname}${url.search}`);
+                break;
+            }
+          })
+          .catch((err) => {
+            const sessionID = localStorage.getItem('session');
+            console.log(err);
+            if (err?.response?.status === 400 && sessionID) {
+              startKYC(did.methodId, sessionID);
+            }
+          });
       };
       const intervalId = setInterval(checkStatus, 5000);
       return () => clearInterval(intervalId);
