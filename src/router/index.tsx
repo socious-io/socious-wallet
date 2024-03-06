4a2b3f31c1e42921fde675637ec7810410ac1fd1
satanmourner <mahmoudisanaz59@gmail.com>
2024-03-06 10:21:04 +0330

    fix: import new pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,47 +1,47 @@
-import { ComponentType } from 'react';
+// import { ComponentType } from 'react';
 import { RouteObject, createBrowserRouter, Navigate } from 'react-router-dom';
-import { useAppState } from 'src/store';
-import Layout from 'src/components/layout';
-import Register from 'src/pages/register';
-import CenteredSpinner from 'src/components/spinner';
-import Recover from 'src/pages/recover';
-import Intro from 'src/pages/intro';
-import Home from 'src/pages/home';
-import Connection from 'src/pages/connection';
-import Verify from 'src/pages/verify';
+import { useAppContext } from 'src/store';
+import Layout from 'src/containers/Layout';
+import Intro from 'src/pages/Intro';
+import Register from 'src/pages/Register';
+import Recover from 'src/pages/Recover';
+import Home from 'src/pages/Home';
+import Connection from 'src/pages/Connection';
+import Verify from 'src/pages/Verify';
+import Loading from 'src/components/Loading';
 
 export const blueprint: RouteObject[] = [
   {
     element: <Layout />,
     children: [
       { path: '/', element: <DefaultRoute /> },
-      { path: '/connect', element: <Connection /> },
       { path: '/intro', element: <Intro /> },
-      { path: '/recover', element: <Recover /> },
       { path: '/register', element: <Register /> },
       { path: '/verify', element: <Verify /> },
+      { path: '/recover', element: <Recover /> },
+      { path: '/connect', element: <Connection /> },
     ],
   },
 ];
 
 function DefaultRoute(): JSX.Element {
-  const state = useAppState();
-  if (state.didLoading) return <CenteredSpinner show={true} />;
+  const { state } = useAppContext();
+  if (state.didLoading) return <Loading show={true} animation="grow" />;
 
   if (!state.did) return <Navigate to="/intro" />;
 
   return <Home />;
 }
 
-function Protect<T extends {}>(Component: ComponentType<T>): ComponentType<T> {
-  return function ProtectedRoute(props: T) {
-    const state = useAppState();
+// function Protect<T extends Record<string, never>>(Component: ComponentType<T>): ComponentType<T> {
+//   return function ProtectedRoute(props: T) {
+//     const state = useAppState();
 
-    if (state.didLoading) return <CenteredSpinner show={true} />;
-    if (!state.did) return <Navigate to="/intro" />;
+//     if (state.didLoading) return <CenteredSpinner show={true} />;
+//     if (!state.did) return <Navigate to="/intro" />;
 
-    return <Component {...props} />;
-  };
-}
+//     return <Component {...props} />;
+//   };
+// }
 
 export default createBrowserRouter(blueprint);
