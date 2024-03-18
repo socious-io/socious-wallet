ef6f2929159acbed11edff6251135686145ca23f
satanmourner <mahmoudisanaz59@gmail.com>
2024-03-18 14:18:44 +0330

    refactor: some texts and styles for recover page

--- src/pages/Recover/index.tsx
+++ src/pages/Recover/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Form, Button } from 'react-bootstrap';
 import ConfirmModal from 'src/components/ConfirmModal';
 import Card from 'src/components/Card';
@@ -6,30 +7,53 @@ import useRecover from './index.services';
 import styles from './index.module.scss';
 
 function Recover() {
+  const navigate = useNavigate();
   const { handleMnemonicValue, onConfirm, disabledConfirm, errorMessage, closeError } = useRecover();
 
   return (
-    <div className="h-100 d-flex align-items-center justify-content-center">
-      <Card containerClassName={styles['card__container']}>
-        <Form onSubmit={onConfirm} className={styles['card__form']}>
-          <h4 className="fw-bold">Secret Recovery Phrase</h4>
-          <span className={styles['card__text']}>Confirm your Secret Recovery Phrase.</span>
-          <Form.Group className="w-100" controlId="recovery">
-            <Form.Control
-              as="textarea"
-              placeholder="Secret Recovery Phrase"
-              rows={6}
-              onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleMnemonicValue(e.target.value)}
-              onPaste={(e: React.ClipboardEvent<HTMLInputElement>) =>
-                handleMnemonicValue(e.clipboardData.getData('text'))
-              }
-            />
-          </Form.Group>
-          <Button className="fw-bold w-100 py-2" variant="primary" type="submit" disabled={disabledConfirm}>
-            Confirm
-          </Button>
-        </Form>
-      </Card>
+    <>
+      <Form onSubmit={onConfirm} className="h-100 d-flex align-items-center justify-content-center">
+        <Card
+          containerClassName={styles['card__container']}
+          contentClassName="h-100 flex-row"
+          buttons={[
+            {
+              children: 'Import wallet',
+              variant: 'primary',
+              type: 'submit',
+              disabled: disabledConfirm,
+              className: 'fw-bold w-100 py-2',
+            },
+            {
+              children: 'Back',
+              variant: 'inherit',
+              type: 'button',
+              onClick: () => navigate('/intro'),
+              className: 'fw-bold w-100 py-2',
+            },
+          ]}
+        >
+          <div className={styles['card__form']}>
+            <div className="d-flex flex-column gap-2 align-items-center">
+              <h4 className="fw-bold">Secret Recovery Phrase</h4>
+              <span className={styles['card__text']}>
+                Restore your existing wallet with your 24-word secret recovery phrase
+              </span>
+              <Form.Group className="my-3 my-md-5 w-100" controlId="recovery">
+                <Form.Control
+                  as="textarea"
+                  placeholder="Secret Recovery Phrase"
+                  rows={6}
+                  onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => handleMnemonicValue(e.target.value)}
+                  onPaste={(e: React.ClipboardEvent<HTMLInputElement>) =>
+                    handleMnemonicValue(e.clipboardData.getData('text'))
+                  }
+                />
+              </Form.Group>
+            </div>
+          </div>
+        </Card>
+      </Form>
       <ConfirmModal
         open={errorMessage !== undefined}
         header="Opps wrong"
@@ -42,7 +66,7 @@ function Recover() {
           <span className={styles['subtitle']}>{errorMessage}</span>
         </div>
       </ConfirmModal>
-    </div>
+    </>
   );
 }
 
