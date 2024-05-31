2cb667786a9b27baf3f12eb72fb43776e019a453
jeyem <me@e-mahmoudi.me>
2024-05-31 09:34:09 +0330

    cap config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,16 @@
 import { CapacitorConfig } from '@capacitor/cli';
+import { Capacitor } from '@capacitor/core';
+
+function defineHostname() {
+  return Capacitor.getPlatform() === 'ios' ? 'socious.io' : 'capacitor.native';
+}
 
 const config: CapacitorConfig = {
   appId: 'socious.wallet.app',
   appName: 'socious-wallet',
   webDir: 'build',
   server: {
+    hostname: defineHostname(),
     androidScheme: 'https',
   },
   plugins: {
