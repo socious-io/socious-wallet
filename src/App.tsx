c38cd27b991f0ef4e3a146999471a61db8388fa8
Jayson Ilagan <jayson.vergara.ilagan@gmail.com>
2024-06-07 22:32:45 +1200

    TAS-1790: Implement deeplinks in Socious wallet

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { RouterProvider } from 'react-router-dom';
 import { AppProvider } from './store';
 // import { Backup } from 'src/services/backup';
 import { Activation } from 'src/services/activation';
+import AppUrlListener from './components/AppUrlListener';
 
 function App() {
   return (
     <AppProvider>
+      <AppUrlListener></AppUrlListener>
       <div className="app__container">
         <Activation />
         <RouterProvider router={routes} />
---
+++ src/components/AppUrlListener/index.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
+
+const AppUrlListener: React.FC<unknown> = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    App.addListener('appUrlOpen', (event: URLOpenListenerEvent) => {
+      const slug = event.url.split('.io').pop();
+      if (slug || slug === '/') {
+        navigate('/intro');
+      }
+    });
+  }, []);
+
+  return null;
+};
+
+export default AppUrlListener;
