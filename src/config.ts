4cd2ffd9bce96248fe6fe0f5456ed905b693c613
jeyem <me@e-mahmoudi.me>
2024-06-26 15:51:05 +0330

    up version

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.2.0';
+export const APP_VERSION = '1.2.1';
 export const config = {
   DEBUG: process.env.REACT_APP_DEBUG,
   SECRET_KEY: process.env.REACT_APP_SECRET_KEY,
