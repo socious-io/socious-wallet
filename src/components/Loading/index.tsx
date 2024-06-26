5f7d2b816c3f0862ee3817ca7d23aa87ae12f3af
satanmourner <mahmoudisanaz59@gmail.com>
2024-06-26 13:32:23 +0330

    refactor: loading according to new design

--- src/components/Loading/index.tsx
+++ src/components/Loading/index.tsx
@@ -4,7 +4,8 @@ import { LoadingProps } from './index.types';
 
 const Loading: React.FC<LoadingProps> = ({
   show,
-  message,
+  title = '',
+  subtitles = [],
   animation = 'border',
   role = 'status',
   variant = 'primary',
@@ -12,9 +13,18 @@ const Loading: React.FC<LoadingProps> = ({
 }) => {
   if (show) {
     return (
-      <div className="h-100 d-flex flex-column gap-5 align-items-center justify-content-center">
-        {message && <h5 className="text-center">{message}</h5>}
+      <div className="h-100 d-flex flex-column align-items-center justify-content-center">
         <Spinner animation={animation} role={role} variant={variant} {...rest} />
+        {title && <h5 className="mt-5 text-center fw-semibold lh-lg">{title}</h5>}
+        {!!subtitles.length && (
+          <div className="d-flex flex-column align-items-center px-2">
+            {subtitles.map(subtitle => (
+              <p key={subtitle} className="text-center text-gray">
+                {subtitle}
+              </p>
+            ))}
+          </div>
+        )}
       </div>
     );
   } else {
