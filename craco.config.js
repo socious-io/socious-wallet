e193d177c23072633e5ca6370169fe4ea1543d16
jeyem <me@e-mahmoudi.me>
2024-09-27 10:30:54 +0330

    fix packages browserify issues

--- craco.config.js
+++ craco.config.js
@@ -1,13 +1,29 @@
+const webpack = require('webpack');
+
 module.exports = {
   webpack: {
-    configure: webpackConfig => {
+    configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
         crypto: require.resolve('crypto-browserify'),
         stream: require.resolve('stream-browserify'),
         path: require.resolve('path-browserify'),
         fs: false,
+        process: require.resolve('process/browser'),  // Polyfill process
       };
+
+      // Fix fully specified issue for 'process/browser'
+      webpackConfig.resolve.alias = {
+        ...webpackConfig.resolve.alias,
+        'process/browser': require.resolve('process/browser'),
+      };
+
+      webpackConfig.plugins.push(
+        new webpack.ProvidePlugin({
+          process: 'process/browser',  // Ensure process is available globally
+        })
+      );
+
       return webpackConfig;
     },
   },
