333d444d144957572f813c7bd878911d8aec3ebf
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-30 01:08:22 +0330

    feat: language switcher on settings page

--- src/pages/Settings/index.tsx
+++ src/pages/Settings/index.tsx
@@ -2,12 +2,22 @@ import NavigationBar from 'src/containers/NavigationBar';
 import Card from 'src/components/Card';
 import Icon from 'src/components/Icon';
 import ConfirmModal from 'src/components/ConfirmModal';
+import PopupModal from 'src/components/PopupModal';
 import useSettings from './index.services';
 import styles from './index.module.scss';
 import cn from 'classnames';
 
 function Settings() {
-  const { translate, settingsItems, openModal, handleCloseModal, handleRemoveWallet } = useSettings();
+  const {
+    translate,
+    settingsItems,
+    languages,
+    isSystemLanguage,
+    onChangeLanguage,
+    openModal,
+    handleCloseModal,
+    handleRemoveWallet,
+  } = useSettings();
 
   return (
     <>
@@ -19,9 +29,21 @@ function Settings() {
           </div>
           <div className={styles['card__content']}>
             {settingsItems.map(item => (
-              <div key={item.title} className={cn(styles['item'], item.action && 'pointer')} onClick={item.action}>
-                {item.title}
-                {item.subtitle && <span className={styles['item__subtitle']}>{item.subtitle}</span>}
+              <div
+                key={item.title}
+                className={cn('w-100 d-flex justify-content-between', item.action && 'pointer')}
+                onClick={item.action}
+              >
+                <div className={styles['item']}>
+                  {item.title}
+                  {item.subtitle && <span className={styles['item__subtitle']}>{item.subtitle}</span>}
+                </div>
+                {item.value && (
+                  <div className={styles['value']}>
+                    {item.value}
+                    <Icon name="chevron-right" />
+                  </div>
+                )}
               </div>
             ))}
           </div>
@@ -54,6 +76,22 @@ function Settings() {
       >
         {translate('settings-confirm.title')}
       </ConfirmModal>
+      <PopupModal
+        open={openModal.name === 'language' && openModal.open}
+        onClose={handleCloseModal}
+        header="Select language"
+      >
+        <div className="d-flex flex-column">
+          {languages.map(lang => (
+            <div key={lang.value} className={styles['lang']} onClick={() => onChangeLanguage(lang.value)}>
+              {lang.label}{' '}
+              <span className={styles['lang--lighter']}>
+                {isSystemLanguage(lang.system) ? '(system)' : lang.original}
+              </span>
+            </div>
+          ))}
+        </div>
+      </PopupModal>
     </>
   );
 }
