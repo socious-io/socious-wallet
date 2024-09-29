b9495e7a8fb5a6aa14d47c021c37c77a11161e36
satanmourner <mahmoudisanaz59@gmail.com>
2024-09-30 00:58:30 +0330

    feat: new words translation for settings and changed default value of language

--- src/store/redux/language.reducers.ts
+++ src/store/redux/language.reducers.ts
@@ -1,11 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 interface LangState {
-  language: 'en' | 'jp';
+  language: 'en' | 'jp' | '';
+  system: 'en' | 'jp' | '';
 }
 
 const initialState = {
-  language: localStorage.getItem('i18nextLng') || 'en',
+  language: localStorage.getItem('i18nextLng') || '',
+  system: localStorage.getItem('system') || '',
 } as LangState;
 
 export const languageSlice = createSlice({
@@ -14,8 +16,17 @@ export const languageSlice = createSlice({
   reducers: {
     setLanguage: (state, action) => {
       state.language = action.payload;
+      state.system = '';
+      localStorage.removeItem('system');
+    },
+    setSystemLanguage: (state, action) => {
+      state.system = action.payload;
+      localStorage.setItem('system', action.payload);
+      if (!state.language) {
+        state.language = action.payload;
+      }
     },
   },
 });
 
-export const { setLanguage } = languageSlice.actions;
+export const { setLanguage, setSystemLanguage } = languageSlice.actions;
