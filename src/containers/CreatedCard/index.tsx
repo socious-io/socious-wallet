262957d7fdee7655215c584ddd19b0ba695ba229
satanmourner <mahmoudisanaz59@gmail.com>
2024-10-01 23:56:33 +0330

    refactor: created page according to new design

--- src/containers/CreatedCard/index.tsx
+++ src/containers/CreatedCard/index.tsx
@@ -4,13 +4,23 @@ import Icon from 'src/components/Icon';
 import { CreatedCardProps } from './index.types';
 import styles from './index.module.scss';
 
-const CreatedCard: React.FC<CreatedCardProps> = ({ title, subtitle, iconName = '', buttons = [] }) => {
+const CreatedCard: React.FC<CreatedCardProps> = ({
+  title,
+  subtitle,
+  iconName = '',
+  buttons = [],
+  contentClassName = '',
+}) => {
   return (
-    <Card buttons={buttons} containerClassName={styles['card__container']}>
+    <Card
+      buttons={buttons}
+      containerClassName={styles['card__container']}
+      contentClassName={`pt-5 text-center ${contentClassName}`}
+    >
       <div className="d-flex flex-column align-items-center gap-2">
         {iconName && <Icon name={iconName} className={styles['card__icon']} />}
-        <h4>{title}</h4>
-        <span className="text-secondary">{subtitle}</span>
+        <h4 className={styles['card__title']}>{title}</h4>
+        <span className={styles['card__subtitle']}>{subtitle}</span>
       </div>
     </Card>
   );
