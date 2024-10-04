9307208306b8f6af4afcd8c9e6c685ce5d582a71
Sanaz Mahmoudi <mahmoudisanaz59@gmail.com>
2024-10-04 11:06:35 +0330

    TAS-2375/Language-Swicther (#42)
    * fix: verified badge just for kyc type
    
    * feat: icon for chevron right
    
    * feat: constants for languages
    
    * feat: popup modal as a general component
    
    * feat: new words translation for settings and changed default value of language
    
    * feat: language switcher on settings page

---
+++ src/constants/languages.ts
@@ -0,0 +1,14 @@
+export const languages = [
+  {
+    value: 'en',
+    label: 'English (US)',
+    original: 'English',
+    system: 'en-US',
+  },
+  {
+    value: 'jp',
+    label: 'Japanese',
+    original: '日本語',
+    system: 'ja',
+  },
+];
--- src/pages/Settings/index.services.ts
+++ src/pages/Settings/index.services.ts
@@ -1,13 +1,22 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { APP_VERSION } from 'src/config';
+import { useDispatch, useSelector } from 'react-redux';
 import { useAppContext } from 'src/store/context';
+import { APP_VERSION } from 'src/config';
+import { RootState } from 'src/store/redux';
+import { setLanguage, setSystemLanguage } from 'src/store/redux/language.reducers';
+import { languages } from 'src/constants/languages';
 
 const useSettings = () => {
   const { t: translate } = useTranslation();
+  const dispatch = useDispatch();
+  const { language, system } = useSelector((state: RootState) => state.language);
   const { state } = useAppContext();
   const { device } = state || {};
-  const [openModal, setOpenModal] = useState<{ name: 'remove' | ''; open: boolean }>({ name: '', open: false });
+  const [openModal, setOpenModal] = useState<{ name: 'remove' | 'language' | ''; open: boolean }>({
+    name: '',
+    open: false,
+  });
   const settingsItems = [
     {
       title: translate('settings-items.remove'),
@@ -22,6 +31,11 @@ const useSettings = () => {
     //   action: () => console.log('terms'),
     // },
     {
+      title: translate('settings-items.language'),
+      value: system ? translate('settings-items.automatic') : languages.find(lang => lang.value === language)?.original,
+      action: () => setOpenModal({ name: 'language', open: true }),
+    },
+    {
       title: translate('settings-items.version'),
       subtitle: APP_VERSION,
     },
@@ -31,7 +45,21 @@ const useSettings = () => {
     },
   ];
 
+  useEffect(() => {
+    if (!language) {
+      const systemLanguage = languages.find(lang => lang.system === navigator.language)?.value || 'en';
+      dispatch(setSystemLanguage(systemLanguage));
+    }
+  }, [language]);
+
   const handleCloseModal = () => setOpenModal({ ...openModal, open: false });
+
+  const isSystemLanguage = language => navigator.language === language;
+
+  const onChangeLanguage = (value: string) => {
+    dispatch(setLanguage(value));
+    handleCloseModal();
+  };
 
   const handleRemoveWallet = async () => {
     const dbs = await indexedDB.databases();
@@ -41,7 +69,16 @@ const useSettings = () => {
     window.location.assign('/intro');
   };
 
-  return { translate, settingsItems, openModal, handleCloseModal, handleRemoveWallet };
+  return {
+    translate,
+    settingsItems,
+    languages,
+    isSystemLanguage,
+    onChangeLanguage,
+    openModal,
+    handleCloseModal,
+    handleRemoveWallet,
+  };
 };
 
 export default useSettings;
