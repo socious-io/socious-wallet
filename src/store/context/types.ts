57cd105613ce7b48c763fd21e0996e3f2b3b7410
satanmourner <mahmoudisanaz59@gmail.com>
2024-10-09 20:49:38 +0330

    feat: add encrypted context for saving in import page

--- src/store/context/types.ts
+++ src/store/context/types.ts
@@ -21,6 +21,7 @@ export interface StateType {
   warn: Err;
   device: DeviceInfo;
   listenerActive: boolean;
+  encrypted: string;
 }
 
 export type ActionType =
@@ -34,10 +35,10 @@ export type ActionType =
   | { type: 'SET_NEW_MESSAGE'; payload: SDK.Domain.Message[] }
   | { type: 'SET_VERIFICATION'; payload: SDK.Domain.Credential }
   | { type: 'SET_SUBMIT'; payload: VerifyStatus }
-  | { type: 'LOADING_END' }
-  | { type: 'LOADING_START' }
   | { type: 'SET_DEVICE'; payload: DeviceInfo }
   | { type: 'SET_LISTENER_STATE'; payload: boolean }
+  | { type: 'SET_ENCRYPTED_DATA'; payload: string }
+  | { type: 'LOADING_START' }
   | { type: 'LOADING_END' };
 
 export type AppProviderProps = { children: React.ReactNode };
