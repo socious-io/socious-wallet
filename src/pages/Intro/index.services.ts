fb2537ec7cf6a4bf9c0209541f76567617d9ca14
satanmourner <mahmoudisanaz59@gmail.com>
2024-10-18 11:10:46 +0330

    refactor: change variables names in intro services

--- src/pages/Intro/index.services.ts
+++ src/pages/Intro/index.services.ts
@@ -20,16 +20,12 @@ export const useIntro = () => {
         accept: ['didcomm/v2'],
         routingKeys: ['did:example:somemediator#somekey'],
       });
-      const {
-        mnemonics: currentMnemonics,
-        privateKey: currentPrivateKey,
-        did: currentDID,
-      } = await createDID([exampleService]);
-      dispatch({ type: 'SET_MNEMONICS', payload: mnemonics });
+      const { mnemonics: newMnemonics, privateKey: newPrivateKey, did: newDID } = await createDID([exampleService]);
+      dispatch({ type: 'SET_MNEMONICS', payload: newMnemonics });
       //FIXME: save into local storage for now
-      localStorage.setItem('mnemonics', currentMnemonics.toString());
-      await pluto.storeDID(currentDID, currentPrivateKey, 'master');
-      dispatch({ type: 'SET_DID', payload: currentDID });
+      localStorage.setItem('mnemonics', newMnemonics.toString());
+      await pluto.storeDID(newDID, newPrivateKey, 'master');
+      dispatch({ type: 'SET_DID', payload: newDID });
       navigate('/setup-pass');
     } catch (e) {
       console.error('Error in creating wallet or saving seed phrase:', e);
