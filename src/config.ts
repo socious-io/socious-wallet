92ac9aaf54bc656ddb188cc336fb75a198b6adce
jeyem <me@e-mahmoudi.me>
2024-10-21 17:52:35 +0330

    version 1.6.10

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.6.9';
+export const APP_VERSION = '1.6.10';
 export const config = {
   ENV: process.env.REACT_APP_ENV || 'development',
   DEBUG: process.env.REACT_APP_DEBUG === 'true' ? true : false,
