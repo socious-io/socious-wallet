f4efac27a4b3fbbf41eff3090ce5dbde6df8c2dc
jeyem <me@e-mahmoudi.me>
2024-10-21 18:11:30 +0330

    download blob on cap

--- src/utilities/downloadLink.ts
+++ src/utilities/downloadLink.ts
@@ -1,13 +1,26 @@
-export const createDownloadLink = (content: BlobPart, filename = 'download', type = 'text/plain') => {
-  const blob = new Blob([content], { type });
-  const url = URL.createObjectURL(blob);
+import { config } from '../config';
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
-  const link = document.createElement('a');
-  link.href = url;
-  link.download = filename;
-  document.body.appendChild(link);
+export const createDownloadLink = async (content: string, filename = 'download', type = 'text/plain') => {
+  if (config.PLATFORM == 'web') {
+    const blob = new Blob([content], { type });
+    const url = URL.createObjectURL(blob);
 
-  link.click();
-  document.body.removeChild(link);
-  URL.revokeObjectURL(url);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = filename;
+    document.body.appendChild(link);
+
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+    return;
+  }
+
+  await Filesystem.writeFile({
+    path: filename,
+    data: content, // Content should be a string for Filesystem
+    directory: Directory.Documents,
+    encoding: Encoding.UTF8,
+  });
 };
