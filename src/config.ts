133b21ae35afe5de0d5ef8dd02e604c90c9ccd63
jeyem <me@e-mahmoudi.me>
2024-12-31 22:02:49 +0330

    release version
    Signed-off-by: jeyem <me@e-mahmoudi.me>

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-export const APP_VERSION = '1.6.20';
+export const APP_VERSION = '1.6.22';
 export const config = {
   ENV: process.env.REACT_APP_ENV || 'development',
   DEBUG: process.env.REACT_APP_DEBUG === 'true' ? true : false,
