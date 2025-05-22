2cdbd7812238ec1cb493d435e43c2a2f9d6fd2c5
Iman rb <iman.rezasefat@gmail.com>
2025-05-22 16:35:49 +0330

    fix file import for web

--- src/pages/Recover/index.services.ts
+++ src/pages/Recover/index.services.ts
@@ -4,6 +4,7 @@ import { useAppContext } from 'src/store/context';
 import { FilePicker } from '@capawesome/capacitor-file-picker';
 import { Filesystem } from '@capacitor/filesystem';
 import { useState } from 'react';
+import { Capacitor } from '@capacitor/core';
 
 const useRecover = () => {
   const { t: translate } = useTranslation();
@@ -15,15 +16,33 @@ const useRecover = () => {
   const onClickUpload = async () => {
     setErrorMessage('');
     try {
-      const result = await FilePicker.pickFiles({
-        types: ['application/octet-stream'],
-      });
+      const result = await FilePicker.pickFiles();
       const file = result.files[0];
       if (file && file.name?.endsWith('.enc')) {
-        const fileContent = await Filesystem.readFile({
-          path: file.path,
-        });
-        const encryptedData = fileContent.data as string;
+        let encryptedData: string;
+
+        if (Capacitor.getPlatform() === 'web') {
+          if (file.blob) {
+            encryptedData = await new Promise((resolve, reject) => {
+              const reader = new FileReader();
+              reader.onload = () => resolve(reader.result as string);
+              reader.onerror = () => reject(reader.error);
+              reader.readAsText(file.blob);
+            });
+          } else {
+            throw new Error('No blob data available');
+          }
+        } else {
+          if (file.path) {
+            const fileContent = await Filesystem.readFile({
+              path: file.path,
+            });
+            encryptedData = fileContent.data as string;
+          } else {
+            throw new Error('No file path available');
+          }
+        }
+
         if (encryptedData) {
           dispatch({ type: 'SET_ENCRYPTED_DATA', payload: encryptedData });
           navigate('/enter-pass');
